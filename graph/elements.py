979275c56d8d6ec3252c2f8308e2b240aa9ea28c
Marco Lanciotti <82757918+marcoLanzo@users.noreply.github.com>
2021-04-18 20:18:26 +0200

    Add files via upload

---
+++ graph/elements.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#encoding: utf-8
+class Node:
+    def __init__(self, index, elem):
+        self.index = index
+        self.elem = elem
+
+    def __str__(self):
+        return  str(self.elem)
+
+class Edge:
+    def __init__(self, tail, head, weight=None):
+        self.head = head  #indice del nodo destinazione
+        self.tail = tail  #indice del nodo sorgente
+        self.weight = weight  #peso dell'arco
+
+
+if __name__ == "__main__":
+    node = Node(0, "casa")
+    print('creato nodo', node)
+    edge = Edge(0, 1, 1.5)
+    print('creato arco (' , edge.tail , ', ' , edge.head, ', ' \
+            , edge.weight , ')')  #TODO: brutto esempio di stampa
